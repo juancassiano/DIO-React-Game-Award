b08ba099f78371f9144584a4fbdcad4b099bc61d
Juan Cassiano <juancassiano@hotmail.com>
2022-12-09 17:54:18 -0300

    Create react project for The Game Award

---
+++ src/components/Votes/GameCard.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, Button, Alert } from 'react-native';
+
+import { clientSendingVotes } from '../../api/api';
+import { gameInterface } from '../../interfaces/gameInterface';
+
+const sendingVotes = (id: number) => {
+  Alert.alert(
+    "Vote Send",
+    "Thanks for helping decide the Game Of The Year",
+    [
+      {text: 'OK', onPress: () => clientSendingVotes(id)}
+    ]
+  )
+}
+
+export function GameCard(props: gameInterface | any){
+  return(
+    <View style={styles.Cardcontainer} key={props.id}>
+      <View>
+        <Image
+          source={{uri:props.cover}}
+          style={styles.card}
+        />
+      </View>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.label}>{props.name}</Text>
+        <Button 
+          onPress={() => {sendingVotes(props.id)}}
+          title="Vote"
+          color='#9AC33C'
+        />
+      </View>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  Cardcontainer:{
+    borderWidth:2,
+    padding:10,
+    borderBottomColor:'#fff',
+    width:'100%',
+    borderRadius:8,
+    flexDirection: 'row',
+    backgroundColor:'#404040'
+  },
+  card:{
+    borderRadius:10,
+    width: 200, 
+    height: 300,
+  },
+  label:{
+    fontSize:22,
+    color:'white',
+    paddingBottom:15,
+    textAlign:'center',
+  },
+  infoContainer:{
+    flex:1,
+    justifyContent:'flex-start',
+    alignItems:'center',
+    textAlign:'center',
+    paddingTop:20,
+  }
+})
